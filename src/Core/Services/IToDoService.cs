5de6e566a0157e113f6195732ead3885d98d7767
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-16 21:05:41 +0800

    Add ToDoService

---
+++ src/Core/Services/IToDoService.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Models;
+
+    public interface IToDoService
+    {
+        /// <summary>
+        /// Retrieve to-do item by account id.
+        /// </summary>
+        /// <param name="accountId">The account id of to-do items to retrieve.</param>
+        /// <returns>A collection of <see cref="ToDoItem"/>.</returns>
+        Task<IEnumerable<ToDoItem>> RetrieveAsync(string accountId);
+
+        /// <summary>
+        /// Retrieve to-do item.
+        /// </summary>
+        /// <param name="accountId">The account id of the to-do item to retrieve.</param>
+        /// <param name="id">The id of the to-do item to retrieve.</param>
+        /// <returns>The <see cref="ToDoItem"/>.</returns>
+        Task<ToDoItem> RetrieveAsync(string accountId, string id);
+    }
+}
---
+++ src/Core/Services/ToDoService.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Models;
+
+    public sealed class ToDoService : IToDoService
+    {
+        ///<inheritdoc/>
+        public Task<IEnumerable<ToDoItem>> RetrieveAsync(string accountId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        ///<inheritdoc/>
+        public Task<ToDoItem> RetrieveAsync(string accountId, string id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
