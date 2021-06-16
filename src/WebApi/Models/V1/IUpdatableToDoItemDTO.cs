3421804c2b782c0f66006ab6e590de4487a6f90b
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-16 22:04:26 +0800

    Add v1 model

---
+++ src/WebApi/Models/V1/IUpdatableToDoItemDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApi.Models.V1
+{
+    /// <summary>
+    /// Define updatable properties of to-do item.
+    /// </summary>
+    public interface IUpdatableToDoItemDTO
+    {
+        /// <summary>
+        /// Get the name of the to-do item.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get the description of the to-do item.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Get the value indicating whether the to-do item is complete.
+        /// </summary>
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ src/WebApi/Models/V1/ToDoItemRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.Models.V1
+{
+    public sealed class ToDoItemRequest : IUpdatableToDoItemDTO
+    {
+        /// <summary>
+        /// Get the name of the to-do item.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get the description of the to-do item.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Get the value indicating whether the to-do item is complete.
+        /// </summary>
+        public bool IsComplete { get; set; }
+    }
+}
