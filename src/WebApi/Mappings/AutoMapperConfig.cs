d0eb0c3384314edc6405ba79bbcdab502361fe7c
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-16 23:14:41 +0800

    Add automapper related configuration

---
+++ src/WebApi/Mappings/AutoMapperConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApi.Mappings
+{
+    using AutoMapper;
+
+    /// <summary>
+    /// Provides configuration for mapper.
+    /// </summary>
+    public static class AutoMapperConfig
+    {
+        /// <summary>
+        /// Initialize mapper.
+        /// </summary>
+        /// <returns>The configured <see cref="IMapper"/>.</returns>
+        public static IMapper Initialize()
+        {
+            var mapperConfig = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new MappingProfile());
+            });
+
+            return mapperConfig.CreateMapper();
+        }
+    }
+}
---
+++ src/WebApi/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.Mappings
+{
+    using AutoMapper;
+    using Core.Models;
+    using WebApi.Models.V1;
+
+    /// <summary>
+    ///  Provides a named configuration for maps.
+    /// </summary>
+    public sealed class MappingProfile : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingProfile"/> class.
+        /// </summary>
+        public MappingProfile()
+        {
+            this.CreateMap<ToDoItem, ToDoItemResponse>().ReverseMap();
+        }
+    }
+}
