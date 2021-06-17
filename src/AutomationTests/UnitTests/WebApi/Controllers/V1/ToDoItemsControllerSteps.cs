188b41d164de35645ec01d62202ec1f92516d481
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-17 22:33:44 +0800

    add ToDoItemsControllerTests

---
+++ src/AutomationTests/UnitTests/WebApi/Controllers/V1/ToDoItemsControllerSteps.cs
@@ -0,0 +1,57 @@
+﻿namespace AutomationTests.UnitTests.WebApi.Controllers.V1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Core.Models;
+    using Core.Services;
+    using FluentAssertions;
+    using global::WebApi.Controllers.V1;
+    using global::WebApi.Mappings;
+    using global::WebApi.Models.V1;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using Xunit;
+
+    internal sealed class ToDoItemsControllerSteps : BaseTestSteps<ToDoItemsControllerSteps>
+    {
+        private readonly Mock<IToDoService> mockService = new Mock<IToDoService>();
+        private readonly IMapper mapper = AutoMapperConfig.Initialize();
+        private readonly ToDoItemsController controller;
+
+        public ToDoItemsControllerSteps()
+        {
+            this.controller = new ToDoItemsController(this.mockService.Object, this.mapper);
+        }
+
+        public ToDoItemsControllerSteps GivenIHaveTheFollowingToDoItems(IEnumerable<ToDoItem> toDoItems)
+        {
+            this.mockService
+                .Setup(x => x.RetrieveAsync(It.IsAny<string>()))
+                .ReturnsAsync(toDoItems);
+
+            return this;
+        }
+
+        public Task WhenICallGet(string accountId)
+        {
+            return this.RecordExceptionAsync(() => this.controller.Get(accountId));
+        }
+
+        public ToDoItemsControllerSteps ThenTheActionResultValueShouldBe(IEnumerable<ToDoItemResponse> expected)
+        {
+            var actionResult = this.Result as ActionResult<IEnumerable<ToDoItemResponse>>;
+            var actionResultValue = actionResult.Value;
+            actionResultValue.Should().BeEquivalentTo(expected, options => options.RespectingRuntimeTypes());
+
+            return this;
+        }
+
+        protected override ToDoItemsControllerSteps GetStepClass()
+        {
+            return this;
+        }
+    }
+}
--- src/AutomationTests/UnitTests/WebApi/Controllers/V1/ToDoItemsControllerTests.cs
+++ src/AutomationTests/UnitTests/WebApi/Controllers/V1/ToDoItemsControllerTests.cs
@@ -1,11 +1,37 @@
 ﻿namespace AutomationTests.UnitTests.WebApi.Controllers.V1
 {
+    using Core.Models;
+    using global::WebApi.Models.V1;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
-    public class ToDoItemsControllerTests
+    using Xunit;
+
+    public sealed class ToDoItemsControllerTests
     {
+        private readonly ToDoItemsControllerSteps steps = new ToDoItemsControllerSteps();
+
+        [Fact]
+        public async Task GetWithAccountId_ToDoItemsFound_Success()
+        {
+            var toDoItems = new[]
+            {
+                new ToDoItem(),
+            };
+
+            var expected = new[]
+            {
+                new ToDoItemResponse(),
+            };
+
+            await this.steps
+                .GivenIHaveTheFollowingToDoItems(toDoItems)
+                .WhenICallGet("account")
+                .ConfigureAwait(false);
+
+            this.steps.ThenTheActionResultValueShouldBe(expected);
+        }
     }
 }
