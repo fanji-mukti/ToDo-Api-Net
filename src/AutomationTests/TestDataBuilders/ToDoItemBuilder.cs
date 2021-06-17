f64455474a5b28f24da610798c9fb243c1c7582d
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-17 22:42:07 +0800

    add ToDoItemBuilder

---
+++ src/AutomationTests/TestDataBuilders/ToDoItemBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Core.Models;
+
+namespace AutomationTests.TestDataBuilders
+{
+    internal sealed class ToDoItemBuilder
+    {
+        private string id = "todo-1";
+        private string accountId = "account-1";
+        private string name = "todo name 1";
+        private string description = "default description";
+        private bool isComplete = true;
+
+        public ToDoItemBuilder WithId(string id)
+        {
+            this.id = id;
+            return this;
+        }
+
+        public ToDoItemBuilder WithAccountId(string accountId)
+        {
+            this.accountId = accountId;
+            return this;
+        }
+
+        public ToDoItemBuilder WithName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public ToDoItemBuilder WithDescription(string description)
+        {
+            this.description = description;
+            return this;
+        }
+
+        public ToDoItemBuilder WithIsComplete(bool isComplete)
+        {
+            this.isComplete = isComplete;
+            return this;
+        }
+
+        public ToDoItem Build()
+        {
+            return new ToDoItem
+            {
+                Id = this.id,
+                AccountId = this.accountId,
+                Name = this.name,
+                Description = this.description,
+                IsComplete = this.isComplete,
+            };
+        }
+    }
+}
