50f2c82d7949eb198907a678d5060205abbd20c1
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-17 21:29:50 +0800

    place holder for ToDoItemsControllerTests

---
+++ src/AutomationTests/UnitTests/WebApi/Controllers/V1/ToDoItemsControllerTests.cs
@@ -0,0 +1,11 @@
+﻿namespace AutomationTests.UnitTests.WebApi.Controllers.V1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ToDoItemsControllerTests
+    {
+    }
+}
