6749b575228dc5223ea5c18e245b604ece827a34
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-18 21:28:46 +0800

    add ToDoItemRequestBuilder

---
+++ src/AutomationTests/TestDataBuilders/ToDoItemRequestBuilder.cs
@@ -0,0 +1,39 @@
+﻿namespace AutomationTests.TestDataBuilders
+{
+    using WebApi.Models.V1;
+
+    internal sealed class ToDoItemRequestBuilder
+    {
+        private string name = "todo name 1";
+        private string description = "default description";
+        private bool isComplete = true;
+
+        public ToDoItemRequestBuilder WithName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public ToDoItemRequestBuilder WithDescription(string description)
+        {
+            this.description = description;
+            return this;
+        }
+
+        public ToDoItemRequestBuilder WithIsComplete(bool isComplete)
+        {
+            this.isComplete = isComplete;
+            return this;
+        }
+
+        public ToDoItemRequest Build()
+        {
+            return new ToDoItemRequest
+            {
+                Name = this.name,
+                Description = this.description,
+                IsComplete = this.isComplete,
+            };
+        }
+    }
+}
