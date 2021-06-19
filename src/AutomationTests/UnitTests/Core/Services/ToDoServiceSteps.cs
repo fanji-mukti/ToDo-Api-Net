7772b050b4dc4cd74fd9101735ba6ca9ffa18b5b
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-19 15:08:43 +0800

    add ToDoServiceSteps

---
+++ src/AutomationTests/UnitTests/Core/Services/ToDoServiceSteps.cs
@@ -0,0 +1,23 @@
+﻿namespace AutomationTests.UnitTests.Core.Services
+{
+    using global::Core.Models;
+    using global::Core.Repositories;
+    using global::Core.Services;
+    using Moq;
+
+    internal sealed class ToDoServiceSteps : BaseTestSteps<ToDoServiceSteps>
+    {
+        private readonly Mock<IRepository<ToDoItem>> mockRepository = new();
+        private readonly ToDoService toDoService;
+
+        public ToDoServiceSteps()
+        {
+            this.toDoService = new ToDoService(this.mockRepository.Object);
+        }
+
+        protected override ToDoServiceSteps GetStepClass()
+        {
+            return this;
+        }
+    }
+}
