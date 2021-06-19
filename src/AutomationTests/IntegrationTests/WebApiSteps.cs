ee1ca0f534262030283080a82a6d54bcf44259c5
fanji-mukti <85936502+fanji-mukti@users.noreply.github.com>
2021-06-19 23:55:51 +0800

    Integration tests (#4)
    * Add From Entity in ToDoItemResponseBuilder
    
    * update connection string to use emulator
    
    * add GetByAccount_ReturnOk
    
    * add GetByAccountAndId_ReturnOk
    
    * add PostAsync_ReturnCreated
    
    * add PutAsync_ReturnNoContent test
    
    * add PatchAsync_ReturnOk
    
    Co-authored-by: Fanji Ari Mukti <fanji.arimukti@outlook.com>

---
+++ src/AutomationTests/IntegrationTests/WebApiSteps.cs
@@ -0,0 +1,107 @@
+﻿namespace AutomationTests.IntegrationTests
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutomationTests.TestHelpers;
+    using Core.Repositories.Entities;
+    using FluentAssertions;
+    using Microsoft.AspNetCore.JsonPatch;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.Azure.Cosmos.Table;
+    using Newtonsoft.Json;
+    using WebApi.Models.V1;
+
+    internal sealed class WebApiSteps : BaseTestSteps<WebApiSteps>
+    {
+        private const string TableName = "ToDoItem";
+
+        private readonly AzureStorageEmulator azureStorageEmulator;
+        private readonly WebApplicationFactory<WebApi.Startup> factory;
+        private readonly HttpClient client;
+        private readonly CloudTable table;
+
+        public WebApiSteps(AzureStorageEmulator azureStorageEmulator, WebApplicationFactory<WebApi.Startup> factory)
+        {
+            this.factory = factory;
+            this.client = factory.CreateClient();
+
+            this.azureStorageEmulator = azureStorageEmulator;
+            this.table = azureStorageEmulator.TableClient.CreateTableIfNotExistsAsync(TableName).GetAwaiter().GetResult();
+        }
+
+        public async Task GivenIHaveTheFollowingEntities(params ToDoItemEntity[] entities)
+        {
+            var operation = new TableBatchOperation();
+
+            foreach (var entity in entities)
+            {
+                operation.Insert(entity);
+            }
+
+            await this.table.ExecuteBatchAsync(operation).ConfigureAwait(false);
+        }
+
+        public Task WhenIGetAsync(string requestUri)
+        {
+            return this.RecordExceptionAsync(() => this.client.GetAsync(requestUri));
+        }
+
+        public Task WhenIPostAsync(string requestUri, ToDoItemRequest request)
+        {
+            var content = CreateStringContent(request);
+            return this.RecordExceptionAsync(() => this.client.PostAsync(requestUri, content));
+        }
+
+        public Task WhenIPutAsync(string requestUri, ToDoItemRequest request)
+        {
+            var content = CreateStringContent(request);
+            return this.RecordExceptionAsync(() => this.client.PutAsync(requestUri, content));
+        }
+
+        public Task WhenIPatchAsync(string requestUri, JsonPatchDocument<IUpdatableToDoItemDTO> jsonPatchDocument)
+        {
+            var content = CreateStringContent(jsonPatchDocument);
+            return this.RecordExceptionAsync(() => this.client.PatchAsync(requestUri, content));
+        }
+
+        public WebApiSteps ThenTheResponseStatusCodeShouldBe(HttpStatusCode expected)
+        {
+            var response = (HttpResponseMessage)this.Result;
+            response.StatusCode.Should().BeEquivalentTo(expected);
+            return this;
+        }
+
+        public async Task ThenTheResponseBodyShouldBe<T>(T expected)
+        { 
+            var responseBody = await GetResponseBodyAsync<T>((HttpResponseMessage)this.Result).ConfigureAwait(false);
+            responseBody.Should().BeEquivalentTo(expected, options => options.RespectingRuntimeTypes());
+        }
+
+        public async Task ThenTheResponseBodyShouldBe(ToDoItemResponse expected, Expression<Func<ToDoItemResponse, object>> excludeExpression)
+        { 
+            var responseBody = await GetResponseBodyAsync<ToDoItemResponse>((HttpResponseMessage)this.Result).ConfigureAwait(false);
+            responseBody.Should().BeEquivalentTo(expected, options => options.Excluding(x => x.Id));
+        }
+
+        protected override WebApiSteps GetStepClass()
+        {
+            return this;
+        }
+
+        private static async Task<T> GetResponseBodyAsync<T>(HttpResponseMessage response)
+        {
+            var stringContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<T>(stringContent);
+        }
+
+        private static StringContent CreateStringContent<T>(T value)
+        {
+            var jsonBody = JsonConvert.SerializeObject(value);
+            return new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        }
+    }
+}
---
+++ src/AutomationTests/IntegrationTests/WebApiTests.cs
@@ -0,0 +1,164 @@
+﻿namespace AutomationTests.IntegrationTests
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Net;
+    using AutomationTests.TestHelpers;
+    using AutomationTests.TestDataBuilders;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Xunit;
+
+    [Collection(nameof(StorageEmulatorCollectionFixture))]
+    public sealed class WebApiTests : IClassFixture<WebApplicationFactory<WebApi.Startup>>
+    {
+        private const string ToDoItemsUriFormat = "api/v1.0/accounts/{0}/ToDoItems";
+        private const string GetByAccountAndIdUriFormat = "api/v1.0/accounts/{0}/ToDoItems/{1}";
+
+        private readonly WebApiSteps steps;
+
+        public WebApiTests(AzureStorageEmulator azureStorageEmulator, WebApplicationFactory<WebApi.Startup> factory)
+        {
+            this.steps = new WebApiSteps(azureStorageEmulator, factory);
+        }
+
+        [Fact]
+        public async Task GetByAccount_ReturnOk()
+        {
+            var accountId = "webapitest-getByaccount";
+
+            var entities = new[]
+            {
+                new ToDoItemEntityBuilder()
+                    .WithRowKey("webapitest-getByAccount-1")
+                    .WithPartitionKey(accountId)
+                    .Build(),
+                new ToDoItemEntityBuilder()
+                    .WithRowKey("webapitest-getByAccount-2")
+                    .WithPartitionKey(accountId)
+                    .Build(),
+            };
+
+            var expected = entities
+                .Select(entity => new ToDoItemResponseBuilder().From(entity).Build());
+
+            var requestUri = String.Format(ToDoItemsUriFormat, accountId);
+
+            await this.steps.GivenIHaveTheFollowingEntities(entities).ConfigureAwait(false);
+            await this.steps.WhenIGetAsync(requestUri).ConfigureAwait(false);
+
+            await this.steps
+                .ThenTheResponseStatusCodeShouldBe(HttpStatusCode.OK)
+                .ThenTheResponseBodyShouldBe(expected)
+                .ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task GetByAccountAndId_ReturnOk()
+        {
+            var accountId = "webapitest-getByAccountAndId";
+
+            var entity = new ToDoItemEntityBuilder()
+                .WithRowKey($"{accountId}-1")
+                .WithPartitionKey(accountId)
+                .Build();
+
+            var expected = new ToDoItemResponseBuilder()
+                .From(entity)
+                .Build();
+
+            var requestUri = String.Format(GetByAccountAndIdUriFormat, accountId, entity.RowKey);
+
+            await this.steps.GivenIHaveTheFollowingEntities(entity).ConfigureAwait(false);
+            await this.steps.WhenIGetAsync(requestUri).ConfigureAwait(false);
+
+            await this.steps
+                .ThenTheResponseStatusCodeShouldBe(HttpStatusCode.OK)
+                .ThenTheResponseBodyShouldBe(expected);
+        }
+
+        [Fact]
+        public async Task PostAsync_ReturnCreated()
+        {
+            var accountId = "webapitest-post";
+
+            var request = new ToDoItemRequestBuilder().Build();
+            var expected = new ToDoItemResponseBuilder()
+                .From(request)
+                .WithAccountId(accountId)
+                .Build();
+
+            var requestUri = String.Format(ToDoItemsUriFormat, accountId);
+
+            await this.steps.WhenIPostAsync(requestUri, request).ConfigureAwait(false);
+
+            await this.steps
+                .ThenTheResponseStatusCodeShouldBe(HttpStatusCode.Created)
+                .ThenTheResponseBodyShouldBe(expected, exclude => exclude.Id);
+        }
+
+        [Fact]
+        public async Task PutAsync_ReturnNoContent()
+        {
+            var accountId = "webapitest-put";
+
+            var entity = new ToDoItemEntityBuilder()
+                .WithRowKey($"{accountId}-1")
+                .WithPartitionKey(accountId)
+                .Build();
+
+            var updateRequest = new ToDoItemRequestBuilder()
+                .WithName("Updated name")
+                .WithDescription("Updated description")
+                .WithIsComplete(false)
+                .Build();
+
+            var requestUri = String.Format(GetByAccountAndIdUriFormat, accountId, entity.RowKey);
+
+            var expected = new ToDoItemResponseBuilder()
+                .From(updateRequest)
+                .WithId(entity.RowKey)
+                .WithAccountId(entity.PartitionKey)
+                .Build();
+
+            await this.steps.GivenIHaveTheFollowingEntities(entity).ConfigureAwait(false);
+            await this.steps.WhenIPutAsync(requestUri, updateRequest).ConfigureAwait(false);
+
+            this.steps.ThenTheResponseStatusCodeShouldBe(HttpStatusCode.NoContent);
+
+            await this.steps.WhenIGetAsync(requestUri).ConfigureAwait(false);
+            await this.steps.ThenTheResponseBodyShouldBe(expected).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task PatchAsync_ReturnOk()
+        {
+            var accountId = "webapitest-patch";
+            var patchedValue = "patched name";
+
+            var entity = new ToDoItemEntityBuilder()
+                .WithRowKey($"{accountId}-1")
+                .WithPartitionKey(accountId)
+                .Build();
+
+            var patchRequest = new ToDoItemPatchRequestBuilder()
+                .WithName(patchedValue)
+                .Build();
+
+            var expected = new ToDoItemResponseBuilder()
+                .From(entity)
+                .WithName(patchedValue)
+                .Build();
+            
+            var requestUri = String.Format(GetByAccountAndIdUriFormat, accountId, entity.RowKey);
+
+            await this.steps.GivenIHaveTheFollowingEntities(entity).ConfigureAwait(false);
+            await this.steps.WhenIPatchAsync(requestUri, patchRequest).ConfigureAwait(false);
+
+            await this.steps
+                .ThenTheResponseStatusCodeShouldBe(HttpStatusCode.OK)
+                .ThenTheResponseBodyShouldBe(expected)
+                .ConfigureAwait(false);
+        }
+    }
+}
