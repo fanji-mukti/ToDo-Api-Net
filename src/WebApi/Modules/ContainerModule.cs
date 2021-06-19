884b5af0649a3be61570c0c48ede5f228dd5d39f
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-19 14:08:22 +0800

    register ToDoItem Repository in module

--- src/WebApi/Modules/ContainerModule.cs
+++ src/WebApi/Modules/ContainerModule.cs
@@ -1,6 +1,8 @@
 ﻿namespace WebApi.Modules
 {
     using Autofac;
+    using Core.Models;
+    using Core.Repositories;
     using Core.Services;
     using WebApi.Mappings;
 
@@ -12,8 +14,15 @@
         /// <inheritdoc/>
         protected override void Load(ContainerBuilder builder)
         {
+            var connectionString = @"DefaultEndpointsProtocol=https;AccountName=muktisandboxstorage;AccountKey=LpYqeX/5+lI74G5mD29wA+Cwzktd3z12vSKsdHkNfQ9jFNblH3zeDoh4o4SwpaZK0hU99PbT1kBn2oBJTTUcyA==;EndpointSuffix=core.windows.net";
+
             builder
-                .Register(c => new ToDoService())
+                .Register(c => RepositoryFactory.CreateToDoItemRepository(connectionString))
+                .As<IRepository<ToDoItem>>()
+                .SingleInstance();
+
+            builder
+                .Register(c => new ToDoService(c.Resolve<IRepository<ToDoItem>>()))
                 .As<IToDoService>();
 
             builder
