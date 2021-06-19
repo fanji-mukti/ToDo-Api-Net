f86a78c3deb2ccb8719a607dc3d2240cae11544b
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2021-06-19 17:28:56 +0800

    add AzureTableRepositoryTests

---
+++ src/AutomationTests/DataAccessLayerTests/AzureTableRepositorySteps.cs
@@ -0,0 +1,28 @@
+﻿namespace AutomationTests.DataAccessLayerTests
+{
+    using System;
+    using AutomationTests.TestHelpers;
+
+    internal sealed class AzureTableRepositorySteps : IDisposable
+    {
+        private readonly AzureStorageEmulator storageEmulator;
+
+        private bool disposed;
+
+        public AzureTableRepositorySteps(AzureStorageEmulator storageEmulator)
+        {
+            this.storageEmulator = storageEmulator;
+        }
+
+        public void Dispose()
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            this.storageEmulator.Dispose();
+            this.disposed = true;
+        }
+    }
+}
---
+++ src/AutomationTests/DataAccessLayerTests/AzureTableRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AutomationTests.DataAccessLayerTests
+{
+    using System;
+
+    using AutomationTests.TestHelpers;
+    using Xunit;
+
+    [Collection(nameof(StorageEmulatorCollectionFixture))]
+    public sealed class AzureTableRepositoryTests : IDisposable
+    {
+        private readonly AzureTableRepositorySteps steps;
+
+        private bool disposed;
+
+        public AzureTableRepositoryTests(AzureStorageEmulator storageEmulator)
+        {
+            this.steps = new AzureTableRepositorySteps(storageEmulator);
+        }
+
+        public void Dispose()
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            this.steps.Dispose();
+            this.disposed = true;
+        }
+    }
+}
